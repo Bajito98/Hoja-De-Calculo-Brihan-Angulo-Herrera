4d24012eec711d33843829d664d009d50cfc6823
LENOVO <LENOVO@Brihan-Mini>
2019-04-20 22:47:03 -0500

    Pueba

---
+++ a/src/vista/InterfazGui.java
@@ -0,0 +1,21 @@
+package vista;
+import javax.swing.JOptionPane;
+
+public class InterfazGui {
+	
+
+	public InterfazGui() {
+		
+	}
+	
+	public String recibirDato(String mensaje) {
+		
+		 String resultado = JOptionPane.showInputDialog(null, mensaje);
+		 return resultado;
+	}
+
+	public void getMensaje(String mensaje) {
+		
+		JOptionPane.showMessageDialog(null, mensaje);
+	}
+}
